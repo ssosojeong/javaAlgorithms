d12694595a2b21983a83c95bff67bf4d7ad6cd97
SOJEONG KIM <thanku1598@gmail.com>
2021-07-12 23:28:10 +0900

    2107121

--- algorithms/src/Middle/example.java
+++ algorithms/src/Middle/example.java
@@ -2,14 +2,7 @@ package Middle;
 
 public class example {
 	public static void main(String[] args) {
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
+		
 		System.out.println();
 	}
 
