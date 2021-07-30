579540ab1becb201adf24111fff9d4718543d76d
SOJEONG KIM <thanku1598@gmail.com>
2021-07-30 23:39:20 +0900

    hey

--- algorithms/src/Middle/example.java
+++ algorithms/src/Middle/example.java
@@ -1,11 +1,18 @@
 package Middle;
 
+import java.util.regex.*;
+
 public class example {
 	public static void main(String[] args) {
 
-		for(int i=5; i<=9; i++) {
-			System.out.println("위시리스트" + i );
-		}
+		System.out.println(isRegexPhone("01089751598"));
 	}
+	
+	public static boolean isRegexPhone(String target) {
+        String regex = "[0-9]{10,11}";
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(target);
+        return matcher.find();
+    }
 
 }
